58c5ab8c60ff59be63d9459fafef91942b1f0355
Zyzle <n1l0c2501@gmail.com>
2015-09-02 01:34:37 +0100

    updated to allow JWT authentication, will use this by default now

--- base/urls.py
+++ base/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 
 from views import home
 
 urlpatterns = patterns('',
     url(r'^$', home),
-    url(r'^api-token-auth/', obtain_auth_token),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
     url(r'snippets/', include('base.apps.snippets.urls', namespace="snippets")),
     url(r'^admin/', include(admin.site.urls)),
 )
