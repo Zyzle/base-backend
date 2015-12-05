99959901274de3bbac95417343f0e9e043bb4a49
Zyzle <n1l0c2501@gmail.com>
2015-12-05 23:52:23 +0000

    updated to django 1.9 and updated urls inline with 1.10 deprecation warning

--- base/apps/snippets/urls.py
+++ base/apps/snippets/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from models import Snippet
 
 from views import get_lexers, SnippetList, SnippetDetail
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^lexers/$', get_lexers),
     url(r'^api/snippets/$', SnippetList.as_view()),
     url(r'^api/snippets/(?P<pk>\d+)/$', SnippetDetail.as_view())
-    )
+]
--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 
 from views import home
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', home),
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
@@ -13,4 +13,4 @@ urlpatterns = patterns('',
     url(r'snippets/', include('base.apps.snippets.urls', namespace="snippets")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^docs/', include('rest_framework_swagger.urls'))
-)
+]
